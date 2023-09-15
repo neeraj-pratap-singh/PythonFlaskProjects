01c34cc744d19131655860d2f91212491456ca70
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-09-15 23:50:34 +0530

    added agent and paragraph scrape code

--- scrapeWebsite.py
+++ scrapeWebsite.py
@@ -7,7 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 
 def fetch_article_titles(url):
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    # Updated headers with Chrome's User-Agent string
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     
     try:
         # Make HTTP request
@@ -24,15 +25,23 @@ def fetch_article_titles(url):
     soup = BeautifulSoup(response.content, "html.parser")
     
     # Find and print article titles
-    # Assuming articles are in h2 tags with class 'entry-title'
-    article_titles = [article.text for article in soup.find_all("h1", class_="entry-title")]
+    # The tag and class should be based on the website structure
+    article_titles = [article.text for article in soup.find_all("h1")]
+    article_paragraph = [article.text for article in soup.find_all("p")]
     
     if article_titles:
         print(f"Found {len(article_titles)} articles.")
         for i, title in enumerate(article_titles, 1):
             print(f"{i}. {title}")
     else:
-        print("No articles found.")
+        print("No articles titles found.")
+
+    if article_paragraph:
+        print(f"Found {len(article_paragraph)} paragraph.")
+        for i, paragraph in enumerate(article_paragraph, 1):
+            print(f"{i}. {paragraph}")
+    else:
+        print("No articles paragraph found.")
 
 if __name__ == "__main__":
     # Replace this URL with the website you want to scrape
